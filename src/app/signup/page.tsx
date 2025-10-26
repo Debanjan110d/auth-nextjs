8dee83c579f6ecb3b3df07ec7208e2c93947315a
Debanjan110d <debanjangamedu@gmail.com>
2025-10-26 23:42:03 +0530

    feat: add login and signup API routes and signup page component

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signuppage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-6xl font-bold text-center">signup page</h1>
+        </div>
+    );
+}
