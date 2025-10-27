54bc591e0beb5107241a337c1f6346e3b7100dae
Debanjan110d <debanjangamedu@gmail.com>
2025-10-28 00:02:25 +0530

    feat: enhance MongoDB connection handling with event listeners

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 
 export async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI as string);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(process.env.MONGODB_URI!);// "!" to assert that the value is not null
+        const connection = mongoose.connection;// This variable holds the dataase connection
+
+        connection.on('connected', () => {
+            console.log('Connected to MongoDB Sucessfully');
+        })
+        connection.on('error', (err) => {
+            console.error('Error connecting to MongoDB:', err);
+            process.exit();
+        })
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
