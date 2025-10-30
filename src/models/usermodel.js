dfc2bf04f941caaf83245cf3f1c6eec25b672cd0
Debanjan110d <debanjangamedu@gmail.com>
2025-10-30 21:50:30 +0530

    feat: add user model with schema for username, email, and password

---
+++ src/models/usermodel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+import { type } from "os";
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    VerifyToken: String,
+    VerifyTokenExpiry: Date
+
+
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema) //? At the end of the day mongodb will make it into lowercase and prural
+
+export default User;
