fc520ea6b58c3b515b223d3e0ca77d61273db40c
Debanjan110d <debanjangamedu@gmail.com>
2025-11-04 23:52:37 +0530

    feat: add middleware for redirecting root path to dashboard and configure path matching

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+
+//? This function can be marked " async" if using `await` inside
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === "/") {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+}
+
+//? See "Matching paths" below to learn more
+export const config = {
+    matcher: [
+        "/",
+        "/profile",
+        "/login",
+        "/signup"
+
+    ],
+};
